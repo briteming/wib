5fb1aa7e27eb335654594f2a7d0e4e583b2ce2b5
Will Schenk <wschenk@gmail.com>
2024-08-28 10:58:06 -0400

    Creating a script to run

---
+++ .github/workflows/create_post_file.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo Title ${TITLE}
+echo Created At ${CREATED_AT}
+echo Labels ${LABELS}
+echo Body ${BODY}
+
+
+POST_FILENAME="_posts/$(date +%Y-%m-%d)-$(echo -n ${TITLE}) | tr '[A-Z]' '[a-z]' | tr -c 'a-z' '-' }}.md"
+
+
+echo --- > ${POST_FILENAME}
+echo title: ${TITLE} >> ${POST_FILENAME}
+echo layout: post >> ${POST_FILENAME}
+echo date: $(date +"%Y-%m-%d %H:%M %z") >> ${POST_FILENAME}
+echo --- >> ${POST_FILENAME}
+echo >> ${POST_FILENAME}
+echo "${BODY}" >> ${POST_FILENAME}
